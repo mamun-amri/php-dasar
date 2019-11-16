95aa780b9dd8a71b888048209510da4b056f9a54
tafrij <mamunamri97@gmail.com>
2019-11-16 11:51:07 +0700

    belajar kondisi

--- pengulangan.php
+++ pengulangan.php
@@ -30,17 +30,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>pengulangan</title>
+    <style>
+        .warna-baris {
+            background-color: silver;
+        }
+
+        .warna-kolom {
+            background-color: linen;
+        }
+    </style>
 </head>
 
 <body>
     <table border="1" cellpadding="10" collpadding="0">
-        <?php for ($i = 1; $i <= 3; $i++) : ?>
-            <tr>
+        <?php for ($i = 1; $i <= 5; $i++) : ?>
+            <?php if ($i % 2 == 1) : ?>
+                <tr class="warna-baris">
+                <?php else : ?>
+                <tr>
+                <?php endif; ?>
                 <?php for ($j = 1; $j <= 5; $j++) : ?>
-                    <td><?= $i . ',' . $j ?></td>
+                    <?php if ($j % 2 == 1) : ?>
+                        <td class="warna-kolom"><?= $i . ',' . $j ?></td>
+                    <?php else : ?>
+                        <td><?= $i . ',' . $j ?></td>
+                    <?php endif; ?>
                 <?php endfor; ?>
-            </tr>
-        <?php endfor; ?>
+                </tr>
+            <?php endfor; ?>
     </table>
 </body>
 
