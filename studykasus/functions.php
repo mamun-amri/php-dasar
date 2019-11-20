2924f7b066b1d31d4df15e8286fa84cdcb5af2cd
tafrij <mamunamri97@gmail.com>
2019-11-21 06:30:01 +0700

    menambahkan fitur login

--- studykasus/functions.php
+++ studykasus/functions.php
@@ -164,3 +164,19 @@ function register($data)
     mysqli_query($conn, "INSERT INTO user VALUES (null,'$username','$password')");
     return mysqli_affected_rows($conn);
 }
+
+function login($data)
+{
+    global $conn;
+    $username = $data['username'];
+    $password = $data['password'];
+
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE username ='$username'");
+    if (mysqli_num_rows($result)) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+            return true;
+            exit;
+        }
+    }
+}
